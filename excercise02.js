7caeb97bea033aa5d5261336802629ba40373e9c
Ligiavelasquez11 <ligiavelasquezbuiles@gmail.com>
2021-10-22 12:06:18 -0500

    Taller Reto

---
+++ excercise02.js
@@ -0,0 +1,9 @@
+const userData = document.getElementById("userData");
+
+const seleccionar = () => {
+    let userName = prompt ("Ingresa tu nombre ");
+}
+
+userData.onclick = () => {
+    seleccionar();
+}
